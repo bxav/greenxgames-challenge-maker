4ea068bb5b6c6d606c9ae4b3082b866cd89a5c4b
Xavier Buillit <x@buill.it>
2018-06-16 05:10:09 +0200

    Remove useless pages and add thing/challenge pages

--- frontend/src/pages/tabs/tabs.ts
+++ frontend/src/pages/tabs/tabs.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
-
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { UserPage } from "../user/user";
+import {ThingPage} from "../thing/thing";
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = ThingPage;
+  tab2Root = UserPage;
 
   constructor() {
 
