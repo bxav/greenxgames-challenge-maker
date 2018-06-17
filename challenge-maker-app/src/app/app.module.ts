45c756c801279879a0d7ce48d82e55cf3ab03f6f
Xavier Buillit <x@buill.it>
2018-06-17 11:43:36 +0200

    Add get things list

--- challenge-maker-app/src/app/app.module.ts
+++ challenge-maker-app/src/app/app.module.ts
@@ -3,19 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { UserPage } from "../pages/user/user";
-import { ThingPage } from "../pages/thing/thing";
 import {RestangularConfigFactory} from "./core/RestangularConfigFactory";
 import {RestangularModule} from "ngx-restangular";
 import {ChallengesService, ThingsService} from "./core/challenge-maker";
 
 import { environment } from '../environments/environment';
-import {QuizPage} from "../pages/quiz/quiz";
 import {AuthService} from "./auth/auth.service";
+import {ThingListPage} from "../pages/thing-list/thing-list";
+import {TruncatePipe} from "../pipes/truncate.pipe";
 
 export function createRestangularConfigFactory(RestangularProvider) {
   return RestangularConfigFactory(RestangularProvider, { baseUrl: environment.greenGameUrl });
@@ -25,10 +22,8 @@ export function createRestangularConfigFactory(RestangularProvider) {
 @NgModule({
   declarations: [
     MyApp,
-    UserPage,
-    TabsPage,
-    ThingPage,
-    QuizPage,
+    ThingListPage,
+    TruncatePipe
   ],
   imports: [
     BrowserModule,
@@ -38,10 +33,7 @@ export function createRestangularConfigFactory(RestangularProvider) {
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    UserPage,
-    TabsPage,
-    ThingPage,
-    QuizPage,
+    ThingListPage
   ],
   providers: [
     StatusBar,
---
+++ challenge-maker-app/src/pages/thing-list/thing-list.ts
@@ -0,0 +1,45 @@
+import {AfterViewInit, Component} from '@angular/core';
+
+import {App, NavController} from 'ionic-angular';
+import {Thing} from "../../app/core/challenge-maker/model";
+import {ThingsService} from "../../app/core/challenge-maker";
+
+@Component({
+  selector: 'page-thing-list',
+  templateUrl: 'thing-list.html'
+})
+export class ThingListPage implements AfterViewInit {
+  protected things: Array<Thing>;
+
+  constructor(
+    private app: App,
+    private thingsService: ThingsService,
+    public navCtrl: NavController
+  ) {
+
+  }
+
+  ngAfterViewInit () {
+    this.app.setTitle('Thing List');
+    this.loadThings();
+  }
+
+  ionViewWillEnter() {
+    console.log("Ionic View Will Enter");
+    this.loadThings();
+  }
+
+  loadThings() {
+    this.thingsService.getAll().subscribe((data: any) => {
+      this.things = data;
+    });
+  }
+
+  thingSelected(thing: Thing) {
+    // this.navCtrl.push(ThingPage, { thingId: thing.id });
+  }
+
+  createThing() {
+    // this.navCtrl.push(ThingPage);
+  }
+}
